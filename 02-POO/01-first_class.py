a1d14f5a7505a8468df5a4fce833a624241170ab
T1A5MA <mmedelm@gmail.com>
2022-03-18 23:13:53 -0300

    Update 01-first_class.py

--- 02-POO/01-first_class.py
+++ 02-POO/01-first_class.py
@@ -3,19 +3,20 @@ class user:
         self.name = name
         self.email = email
         self.account_balance = 0
-    
+
     def make_deposit(self, amount):
         self.account_balance += amount
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
-    
+
     def display_user_balance(self):
         print(f"User: {self.name}, Saldo: {self.account_balance}")
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.make_deposit(amount)
+
 
 matias = user("Matias", "mmedelm@gmail.com")
 james = user("James", "james@smth.com")
